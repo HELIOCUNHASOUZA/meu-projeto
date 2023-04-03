f674e0d163f06dc1e380e0eede8f65d8c99ff81b
helio Cunha <heliocunha.souza@gmail.com>
2023-04-03 14:01:46 -0300

    html e css feito

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express= require("express");
 const app = express();
 const port = 3000;
 const router = require("./router")
+const path = require("path")
+
+
+app.use(express.static(path.join(__dirname, "public")))
 
 //definindo router para gerenciar rotas 
 app.use("/", router)
